5bb6628cc719ce7a3f6da04cebc462a76b593bc4
guoj <dev.guoj@gmail.com>
2017-06-14 14:28:42 +0800

    interp with closure

---
+++ interp-with-closure.rkt
@@ -0,0 +1,70 @@
+#lang plai-typed
+
+;;; init env
+
+(define-type Binding
+  [bind (name : symbol) (val : Value)])
+(define-type-alias Env (listof Binding))
+(define mt-env empty)
+(define extend-env cons)
+
+;;; data types
+
+(define-type Value
+  [numV (n : number)]
+  [closV (arg : symbol) (body : ExprC) (env : Env)])
+
+(define-type ExprC
+  [numC (n : number)]
+  [idC (s : symbol)]
+  [appC (fun : ExprC) (arg : ExprC)]
+  [plusC (l : ExprC) (r : ExprC)]
+  [multC (l : ExprC) (r : ExprC)]
+  [lamC (arg : symbol) (body : ExprC)])
+
+;;; helper functions
+
+(define (lookup [exp : symbol] [env : Env]) : Value
+  (cond
+    [(empty? env) (error 'lookup "name not found")]
+    [else (cond
+            [(symbol=? exp (bind-name (first env)))
+             (bind-val (first env))]
+            [else (lookup exp (rest env))])]))
+
+;;; interpreter
+
+(define (interp [expr : ExprC] [env : Env]) : Value
+  (type-case ExprC expr
+    [numC (n) (numV n)]
+    [idC (n) (lookup n env)]
+    [appC (f a) (local ([define f-value (interp f env)])
+                  (interp (closV-body f-value)
+                          (extend-env (bind (closV-arg f-value)
+                                            (interp a env))
+                                      env)))]
+    [plusC (l r) (num+ (interp l env) (interp r env))]
+    [multC (l r) (num* (interp l env) (interp r env))]
+    [lamC (a b) (closV a b env)]))
+
+(define (num+ [l : Value] [r : Value]) : Value
+  (cond
+    [(and (numV? l) (numV? r))
+     (numV (+ (numV-n l) (numV-n r)))]
+    [else (error 'num+ "one argument was not a number")]))
+
+(define (num* [l : Value] [r : Value]) : Value
+  (cond
+    [(and (numV? l) (numV? r))
+     (numV (* (numV-n l) (numV-n r)))]
+    [else (error 'num* "one argument was not a number")]))
+
+;;; tests
+
+(test (interp (multC
+               (plusC (numC 100)
+                      (appC (lamC 'x (multC (idC 'x) (idC 'x)))
+                            (numC 20)))
+               (numC 2))
+              mt-env)
+      (numV 1000))
